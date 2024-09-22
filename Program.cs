44e58e11da7c44981c4d01f6d20b62458082fe4a
alis-ssss <154761093+alis-ssss@users.noreply.github.com>
2024-09-22 22:31:03 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
-﻿// Консоль выведет 0
-// потому что в заданном выражении:
-// c = (31 – 5 * a) / b = (31 - 5 * 10) / 38 = -19 / 38 = -0.5
-// Получилось с = -0.5, но т.к. переменная объявлена целым числом,
-// то результат деления округляется и становится равным 0.
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество пациентов: ");
+            int numberOfPatients = Convert.ToInt32(Console.ReadLine());
+
+            int receptionTime = 10;
+            int totalWaitingTime = numberOfPatients * receptionTime;
+            int waitingHours = totalWaitingTime / 60;
+            int waitingMinutes = totalWaitingTime % 60;
+
+            Console.WriteLine($"Вам предстоит ждать {waitingHours} часа и {waitingMinutes} минут.");
+            Console.ReadKey();
+        }
+    }
+}
